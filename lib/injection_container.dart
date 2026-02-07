eb17ce7de7a136af4fb63cda5bde9873e6dee8eb
Artenlaclase <artenlaclase@gmail.com>
2026-02-07 13:32:46 -0300

    Add connectivity_plus integration and setup dependency injection

---
+++ lib/injection_container.dart
@@ -0,0 +1,17 @@
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+void init() {
+  // BLoC
+
+  // Use cases
+
+  // Repository
+
+  // Data sources
+
+  // Core
+
+  // External
+}
